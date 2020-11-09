8527ab73c027a9f6e9bc82172ae44e3b24305a28
Toni Rämö <toni.ramo@helsinki.fi>
2020-11-09 12:08:07 +0200

    follow stylerules

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -8,22 +8,12 @@ public class Varasto {
 
     // --- konstruktorit: ---
     public Varasto(double tilavuus) {  // tilavuus on annettava
-        if (tilavuus > 0.0) 
-        {
-            if (tilavuus == -1 ) { //peräkkäinen if-statement
-                return;
-            }
+        if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
         } else { // virheellinen, nollataan 
             this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
-            saldo = 0;     // oletus: varasto on tyhjä
-
-        for( int i=0; i<1; i++ ) {
-            for( int j=0; i<j; j++ ) {
-              System.out.println("virhe");
-            } 
-          }
+        saldo = 0;     // oletus: varasto on tyhjä
     }
 
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
@@ -32,14 +22,18 @@ public class Varasto {
         } else { // virheellinen, nollataan 
             this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
+        lisaaVarastoon(alkuSaldo);
+        /*
         if (alkuSaldo < 0.0) {
             this.saldo = 0.0;
         } else if (alkuSaldo <= tilavuus) { // mahtuu
             this.saldo = alkuSaldo;
         } else {
             this.saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
-        }
+        }*/
     }
+
+    
 
     // --- ottavat aksessorit eli getterit: ---
     public double getSaldo() {
@@ -86,44 +80,4 @@ public class Varasto {
         return ("saldo = " + saldo + ", vielä tilaa " + paljonkoMahtuu());
     }
 
-    public void turhaMetodi() {
-        int x = 0;
-        if (x == 0) {
-            return;
-        } else if (x == 1) {
-            System.out.print("pöö");
-
-            return;
-        }
-
-        else if (x == 2) {
-            System.out.print("pöö");
-
-            return;
-        }
-
-        else if (x == 3) {
-            return;
-        }
-
-        else if (x == 4) {
-            return;
-        }
-
-        else if (x == 5) {
-            return;
-        }
-
-        else if (x == 6) {
-            return;
-        }
-
-        else if (x == 7) {
-            System.out.print("pöö");
-            return;
-        } else {
-            return;
-        }
-
-    }
 }
